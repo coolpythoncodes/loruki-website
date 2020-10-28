56debcd796ae988fbc243672c4da30b6fcd5535f
Godson Rapture Chijioke <coolpythoncodes@gmail.com>
2020-10-28 19:49:25 +0100

    added navbar section

---
+++ src/component/Showcase.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Showcase = () => {
+    return (
+			<section className="showcase">
+				<div className="container grid">
+					<div className="showcase-text">
+                        <h1>Easier Deployment</h1>
+                        <p>Deploy web apps of all kinds, from large </p>
+                    </div>
+				</div>
+			</section>
+		);
+}
+
+export default Showcase;
